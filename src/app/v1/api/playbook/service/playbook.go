66f7d8d4c32cdba86a2cfd8328cf5a16d164bf50
sofyan48 <sofyan.saputra@ralali.com>
2020-05-01 19:01:17 +0700

    Playbook execute fix

--- src/app/v1/api/playbook/service/playbook.go
+++ src/app/v1/api/playbook/service/playbook.go
@@ -29,6 +29,6 @@ func (service *PlaybookService) PlaybookService(app, conn, inventory string) err
 	invent := service.Ansible.SetInventory(inventory)
 	playbookStorage := os.Getenv("PLAYBOOK_PATH")
 	playbookPath := playbookStorage + "/" + app + "/main.yml"
-	err := service.Ansible.Execute(connection, invent, playbookPath, "Runn Example")
+	err := service.Ansible.Execute(connection, invent, playbookPath, "")
 	return err
 }
